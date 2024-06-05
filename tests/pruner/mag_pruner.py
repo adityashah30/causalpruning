15dcdfbbcc461f66e20af90fd830aa6dac649a6a
Aditya Shah <adityashah30@gmail.com>
2024-06-05 08:59:40 -0500

    Updates MagPruner to the correct implementation and adds it to dev.ipynb

--- tests/pruner/mag_pruner.py
+++ tests/pruner/mag_pruner.py
@@ -8,18 +8,17 @@ from ..context import causalpruner
 
 
 class MagPruner(causalpruner.Pruner):
-    def __init__(self, model: nn.Module,
-                 amount: float = 0.4,
+    def __init__(self, model: nn.Module, amount: float = 0.4,
                  device: Union[str, torch.device] = causalpruner.best_device()):
         super().__init__(model, device)
         self.amount = amount
 
     def compute_masks(self) -> None:
-        parameters_to_prune = []
-        for param in self.params_dict.values():
-            parameters_to_prune.append(param)
+        params_to_prune = []
+        for param in self.params:
+            params_to_prune.append((self.modules_dict[param], 'weight'))
         prune.global_unstructured(
-            parameters_to_prune,
+            params_to_prune,
             pruning_method=prune.L1Unstructured,
             amount=self.amount,
         )
