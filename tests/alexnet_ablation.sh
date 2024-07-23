88bec9db0fcbd6bebb9a9175a83ef7f50404cf78
Aditya Shah <adityashah30@gmail.com>
2024-07-22 22:51:43 -0500

    Adds ablation studies + cleans up exp.sh + adds resnet18

--- tests/alexnet_ablation.sh
+++ tests/alexnet_ablation.sh
@@ -14,7 +14,7 @@ for iter in ${num_iterations[@]}; do
             echo "Num iterations: ${iter}; Num prune epochs: ${prune_epochs}; alpha: ${alpha}"
             python main.py --model=alexnet --dataset=cifar10 \
                 --prune --pruner=causalpruner \
-                ----causal_pruner_batch_size=16 \
+                --causal_pruner_batch_size=16 \
                 --num_prune_iterations="${iter}" \
                 --num_prune_epochs="${prune_epochs}" \
                 --causal_pruner_l1_regularization_coeff="${alpha}"
