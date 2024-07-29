a2ba161564f7664803eda8e2512003fa6241bb35
Aditya Shah <adityashah30@gmail.com>
2024-07-29 11:27:32 -0500

    Adds correct transforms for data

--- tests/datasets/imagenet.py
+++ tests/datasets/imagenet.py
@@ -1,8 +1,8 @@
 import os
 import torch
 import torch.utils.data as data
-from torchvision.models import ResNet50_Weights
 from torchvision.datasets import ImageNet
+from torchvision.transforms import v2
 
 
 @torch.no_grad
@@ -12,10 +12,17 @@ def get_imagenet(
     model_name = model_name.lower()
     imagenet_root = os.path.join(root_dir, 'imagenet')
     if model_name == 'resnet50':
-        transforms = ResNet50_Weights.IMAGENET1K_V2.transforms
+        transforms = v2.Compose([
+            v2.RandomResizedCrop(224),
+            v2.RandomHorizontalFlip(),
+            v2.ToImage(),
+            v2.ToDtype(torch.float32, scale=True),
+            v2.Normalize(
+                mean=[0.485, 0.456, 0.406],
+                std=[0.229, 0.224, 0.225])])
         train = ImageNet(root=imagenet_root, split='train',
                          transform=transforms)
         test = ImageNet(root=imagenet_root, split='val',
-                        transforms=transforms)
+                        transform=transforms)
         return (train, test)
     raise NotImplementedError(f'CIFAR10 not available for {model_name}')
