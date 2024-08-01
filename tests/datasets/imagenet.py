51a2bf55eed3d57651c294a692227eee60e6256e
Aditya Shah <adityashah30@gmail.com>
2024-08-01 10:01:53 -0500

    Adds back v2.Image to transforms and splits train and test transforms

--- tests/datasets/imagenet.py
+++ tests/datasets/imagenet.py
@@ -12,16 +12,20 @@ def get_imagenet(
     model_name = model_name.lower()
     imagenet_root = os.path.join(root_dir, 'imagenet')
     if model_name == 'resnet50':
-        transforms = v2.Compose([
-            v2.RandomResizedCrop(224),
-            v2.RandomHorizontalFlip(),
+        default_transforms = [
+            v2.ToImage(),
             v2.ToDtype(torch.float32, scale=True),
             v2.Normalize(
                 mean=[0.485, 0.456, 0.406],
-                std=[0.229, 0.224, 0.225])])
+                std=[0.229, 0.224, 0.225])]
+        train_transforms = v2.Compose([
+            v2.RandomResizedCrop(224),
+            v2.RandomHorizontalFlip(),
+        ] + default_transforms)
+        test_transforms = v2.Compose(default_transforms)
         train = ImageNet(root=imagenet_root, split='train',
-                         transform=transforms)
+                         transform=train_transforms)
         test = ImageNet(root=imagenet_root, split='val',
-                        transform=transforms)
+                        transform=test_transforms)
         return (train, test)
     raise NotImplementedError(f'CIFAR10 not available for {model_name}')
