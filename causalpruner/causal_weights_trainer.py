Corrects gradient accumulation

causalpruner/causal_weights_trainer.py
@@ -120,7 +120,8 @@ class CausalWeightsTrainerTorch(CausalWeightsTrainer):
         self.layer.train()
         best_loss = np.inf
         iter_no_change = 0
-        for iter in trange(self.max_iter, leave=False):
+        for iter in trange(
+                self.max_iter, leave=False, desc=f'Prune weight fitting'):
             sumloss = 0.0
             pbar_prune = tqdm(dataloader, leave=False)
             for idx, (X, Y) in enumerate(pbar_prune):

causalpruner/sgd_pruner.py
@@ -141,9 +141,9 @@ class SGDPruner(Pruner):
         self.checkpoint_futures = []
 
     @torch.no_grad
-    def provide_loss(self, loss: torch.Tensor, steps: int = 1) -> None:
-        loss = (loss).detach().cpu() / steps
-        self.write_tensor(loss, self._get_checkpoint_path('loss'))
+    def provide_loss(self, loss: float) -> None:
+        self.write_tensor(torch.tensor(
+            loss), self._get_checkpoint_path('loss'))
         for param in self.params:
             module = self.modules_dict[param]
             weight = module.weight.detach().to(device='cpu', non_blocking=True)