499eb1a47eb2f639a863ef70c68d1d1db2acdd86
Aditya Shah <adityashah30@gmail.com>
2025-02-02 14:16:56 -0600

    Updates setup script that fetches LFS file, sets up conda env

--- setup.sh
+++ setup.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+git lfs pull
+
 mkdir -p ./data
 mkdir -p ./checkpoints
 mkdir -p ./tensorboard
 
-tar -xvzf ./data/tinyimagenet200.tar.gz -C ./data/tinyimagenet200
+tar -xvzf ./data/tinyimagenet200.tar.gz -C ./data
+
+conda env create -f environment.yml
+conda activate cpn
 
 python datasets_setup.py
