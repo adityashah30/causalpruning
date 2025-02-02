030ded90a515c5df9a9b96b930d0ebb0f30181f4
Aditya Shah <adityashah30@gmail.com>
2025-02-02 14:20:45 -0600

    Updates the setup script to use conda run which is the correct programmatic approach

--- setup.sh
+++ setup.sh
@@ -9,6 +9,5 @@ mkdir -p ./tensorboard
 tar -xvzf ./data/tinyimagenet200.tar.gz -C ./data
 
 conda env create -f environment.yml
-conda activate cpn
 
-python datasets_setup.py
+conda run -n cpn --live-stream python datasets_setup.py
