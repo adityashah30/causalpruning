Removes caching from datasets and corrects deltacomputer computation

causalpruner/sgd_pruner.py
@@ -60,18 +60,18 @@ class DeltaComputer:
 
     @torch.no_grad()
     def add_first(self, weight: torch.Tensor):
-        self.first_tensor = self.transform(weight)
+        self.first_tensor = weight
 
     @torch.no_grad()
     def add_second(self, weight: torch.Tensor):
-        self.second_tensor = self.transform(weight)
+        self.second_tensor = weight
 
     @torch.no_grad()
     def get_delta(self) -> Optional[torch.Tensor]:
         if self.first_tensor is None or self.second_tensor is None:
             return None
         delta = self.second_tensor - self.first_tensor
-        return delta
+        return self.transform(delta)
 
 
 @dataclass
@@ -107,8 +107,7 @@ class SGDPruner(Pruner):
             self.weights_checkpoint_dir, f"{self.iteration}"
         )
         self.delta_weights_computer = DeltaComputer(transform=torch.square)
-        self.loss_dir = os.path.join(
-            self.loss_checkpoint_dir, f"{self.iteration}")
+        self.loss_dir = os.path.join(self.loss_checkpoint_dir, f"{self.iteration}")
         self.delta_loss_computer = DeltaComputer()
         self.checkpoint_futures = []
         self.counter = 0
@@ -152,8 +151,7 @@ class SGDPruner(Pruner):
 
         delta_loss = self.delta_loss_computer.get_delta()
         if delta_loss is not None:
-            self.write_tensor(
-                delta_loss, self._get_checkpoint_path(self.loss_dir))
+            self.write_tensor(delta_loss, self._get_checkpoint_path(self.loss_dir))
 
         delta_weights = self.delta_weights_computer.get_delta()
         if delta_weights is not None:

tests/datasets/__init__.py
@@ -8,27 +8,21 @@ import torch.utils.data as data
 
 
 @torch.no_grad
-def get_dataset(dataset_name: str,
-                model_name: str,
-                data_root_dir: str,
-                cache_size_limit_gb: int = 8) -> tuple[
-                    data.Dataset, data.Dataset, int]:
+def get_dataset(
+    dataset_name: str, model_name: str, data_root_dir: str
+) -> tuple[data.Dataset, data.Dataset, int]:
     model_name = model_name.lower()
     dataset_name = dataset_name.lower()
-    if dataset_name == 'cifar10':
-        train, test = get_cifar_10(
-            model_name, data_root_dir, cache_size_limit_gb)
+    if dataset_name == "cifar10":
+        train, test = get_cifar_10(model_name, data_root_dir)
         return (train, test, 10)
-    elif dataset_name == 'fashionmnist':
-        train, test = get_fashion_mnist(
-            model_name, data_root_dir, cache_size_limit_gb)
+    elif dataset_name == "fashionmnist":
+        train, test = get_fashion_mnist(model_name, data_root_dir)
         return (train, test, 10)
-    elif dataset_name == 'tinyimagenet':
-        train, test = get_tiny_imagenet(
-            model_name, data_root_dir, cache_size_limit_gb)
+    elif dataset_name == "tinyimagenet":
+        train, test = get_tiny_imagenet(model_name, data_root_dir)
         return (train, test, 200)
-    elif dataset_name == 'imagenet':
-        train, test = get_imagenet(
-            model_name, data_root_dir)
+    elif dataset_name == "imagenet":
+        train, test = get_imagenet(model_name, data_root_dir)
         return (train, test, 1000)
-    raise NotImplementedError(f'{dataset_name} not available.')
+    raise NotImplementedError(f"{dataset_name} not available.")

tests/datasets/cifar10.py
@@ -1,7 +1,5 @@
 import os
-from typing import Optional, Callable
 
-from stocaching import SharedCache
 import torch
 import torch.utils.data as data
 from torchvision.datasets import CIFAR10
@@ -9,69 +7,33 @@ from torchvision.transforms import v2
 
 
 _DEFAULT_TRAIN_TRANSFORMS = [
+    v2.RandomCrop(32),
     v2.RandomHorizontalFlip(),
     v2.ToImage(),
     v2.ToDtype(torch.float32, scale=True),
-    v2.Normalize(
-        mean=(0.4914, 0.4822, 0.4465),
-        std=(0.2023, 0.1994, 0.2010))
+    v2.Normalize(mean=(0.4914, 0.4822, 0.4465), std=(0.2023, 0.1994, 0.2010)),
 ]
 _DEFAULT_TEST_TRANSFORMS = [
     v2.ToImage(),
     v2.ToDtype(torch.float32, scale=True),
-    v2.Normalize(
-        mean=(0.4914, 0.4822, 0.4465),
-        std=(0.2023, 0.1994, 0.2010))
+    v2.Normalize(mean=(0.4914, 0.4822, 0.4465), std=(0.2023, 0.1994, 0.2010)),
 ]
-
-
-class CachedCIFAR10(CIFAR10):
-
-    def __init__(self,
-                 root_dir: str,
-                 size: int,
-                 train: bool,
-                 transform: Optional[Callable],
-                 download: bool,
-                 cache_size_limit_gb: int):
-        super().__init__(root_dir, train, transform, download=download)
-        num_items = super().__len__()
-        self.input_cache = SharedCache(
-            size_limit_gib=cache_size_limit_gb,
-            dataset_len=num_items,
-            data_dims=(3, size, size),
-            dtype=torch.float32)
-
-    def __getitem__(self, idx: int) -> tuple[torch.Tensor, torch.Tensor]:
-        inputs = self.input_cache.get_slot(idx)
-        if inputs is None:
-            inputs, _ = super().__getitem__(idx)
-            self.input_cache.set_slot(idx, inputs)
-        labels = self.targets[idx]
-        return (inputs, labels)
 
 
 @torch.no_grad
 def get_cifar_10(
-        model_name: str,
-        data_root_dir: str,
-        cache_size_limit_gb: int) -> tuple[data.Dataset, data.Dataset]:
+    model_name: str, data_root_dir: str
+) -> tuple[data.Dataset, data.Dataset]:
     model_name = model_name.lower()
-    cifar_root_dir = os.path.join(data_root_dir, 'cifar10')
-    if model_name in ['alexnet', 'lenet', 'resnet18']:
+    cifar_root_dir = os.path.join(data_root_dir, "cifar10")
+    if model_name in ["alexnet", "lenet", "resnet18"]:
         train_transforms = v2.Compose(_DEFAULT_TRAIN_TRANSFORMS)
         test_transforms = v2.Compose(_DEFAULT_TEST_TRANSFORMS)
-        train = CachedCIFAR10(cifar_root_dir,
-                              size=32,
-                              train=True,
-                              transform=train_transforms,
-                              download=True,
-                              cache_size_limit_gb=cache_size_limit_gb)
-        test = CachedCIFAR10(cifar_root_dir,
-                             size=32,
-                             train=False,
-                             transform=test_transforms,
-                             download=True,
-                             cache_size_limit_gb=cache_size_limit_gb)
+        train = CIFAR10(
+            cifar_root_dir, train=True, download=True, transform=train_transforms
+        )
+        test = CIFAR10(
+            cifar_root_dir, train=False, download=True, transform=test_transforms
+        )
         return (train, test)
-    raise NotImplementedError(f'CIFAR10 not available for {model_name}')
+    raise NotImplementedError(f"CIFAR10 not available for {model_name}")

tests/datasets/fashion_mnist.py
@@ -1,7 +1,5 @@
 import os
-from typing import Optional, Callable
 
-from stocaching import SharedCache
 import torch
 import torch.utils.data as data
 from torchvision.datasets import FashionMNIST
@@ -11,70 +9,32 @@ from torchvision.transforms import v2
 _DEFAULT_TRANSFORMS = [
     v2.ToImage(),
     v2.ToDtype(torch.float32, scale=True),
-    v2.Normalize(mean=(0.2860,), std=(0.3530,))
+    v2.Normalize(mean=(0.2860,), std=(0.3530,)),
 ]
 
 
-class CachedFashionMNIST(FashionMNIST):
-
-    def __init__(self,
-                 root_dir: str,
-                 size: int,
-                 train: bool,
-                 transform: Optional[Callable],
-                 download: bool,
-                 cache_size_limit_gb):
-        super().__init__(root_dir, train, transform, download=download)
-        num_items = super().__len__()
-        self.input_cache = SharedCache(
-            size_limit_gib=cache_size_limit_gb,
-            dataset_len=num_items,
-            data_dims=(1, size, size),
-            dtype=torch.float32)
-
-    def __getitem__(self, idx: int) -> tuple[torch.Tensor, torch.Tensor]:
-        inputs = self.input_cache.get_slot(idx)
-        if inputs is None:
-            inputs, _ = super().__getitem__(idx)
-            self.input_cache.set_slot(idx, inputs)
-        labels = self.targets[idx]
-        return (inputs, labels)
-
 @torch.no_grad
 def get_fashion_mnist(
-        model_name: str,
-        data_root_dir: str,
-        cache_size_limit_gb: int) -> tuple[data.Dataset, data.Dataset]:
+    model_name: str, data_root_dir: str
+) -> tuple[data.Dataset, data.Dataset]:
     model_name = model_name.lower()
-    fashionmnist_root_dir = os.path.join(data_root_dir, 'fashionmnist')
-    if model_name == 'lenet':
+    fashionmnist_root_dir = os.path.join(data_root_dir, "fashionmnist")
+    if model_name == "lenet":
         transform = v2.Compose(_DEFAULT_TRANSFORMS)
-        train = CachedFashionMNIST(fashionmnist_root_dir,
-                                   size=28,
-                                   train=True,
-                                   transform=transform,
-                                   download=True,
-                                   cache_size_limit_gb=cache_size_limit_gb)
-        test = CachedFashionMNIST(fashionmnist_root_dir,
-                                  size=28,
-                                  train=False,
-                                  transform=transform,
-                                  download=True,
-                                  cache_size_limit_gb=cache_size_limit_gb)
+        train = FashionMNIST(
+            fashionmnist_root_dir, train=True, download=True, transform=transform
+        )
+        test = FashionMNIST(
+            fashionmnist_root_dir, train=False, download=True, transform=transform
+        )
         return (train, test)
-    elif model_name in ['alexnet', 'resnet18']:
+    elif model_name in ["alexnet", "resnet18"]:
         transform = v2.Compose([v2.Resize((32, 32))] + _DEFAULT_TRANSFORMS)
-        train = CachedFashionMNIST(fashionmnist_root_dir,
-                                   size=32,
-                                   train=True,
-                                   transform=transform,
-                                   download=True,
-                                   cache_size_limit_gb=cache_size_limit_gb)
-        test = CachedFashionMNIST(fashionmnist_root_dir,
-                                  size=32,
-                                  train=False,
-                                  transform=transform,
-                                  download=True,
-                                  cache_size_limit_gb=cache_size_limit_gb)
+        train = FashionMNIST(
+            fashionmnist_root_dir, train=True, download=True, transform=transform
+        )
+        test = FashionMNIST(
+            fashionmnist_root_dir, train=False, download=True, transform=transform
+        )
         return (train, test)
-    raise NotImplementedError(f'CIFAR10 not available for {model_name}')
+    raise NotImplementedError(f"CIFAR10 not available for {model_name}")

tests/datasets/tinyimagenet.py
@@ -1,7 +1,6 @@
 import os
-from typing import Optional, Callable
+from typing import Callable, Optional
 
-from stocaching import SharedCache
 import torch
 import torch.utils.data as data
 from torchvision.datasets import ImageFolder
@@ -12,97 +11,80 @@ _DEFAULT_TRAIN_TRANSFORMS = [
     v2.RandomHorizontalFlip(),
     v2.ToImage(),
     v2.ToDtype(torch.float32, scale=True),
-    v2.Normalize(
-        mean=(0.4802, 0.4481, 0.3975),
-        std=(0.2764, 0.2689, 0.2816))
+    v2.Normalize(mean=(0.4802, 0.4481, 0.3975), std=(0.2764, 0.2689, 0.2816)),
 ]
 _DEFAULT_TEST_TRANSFORMS = [
     v2.ToImage(),
     v2.ToDtype(torch.float32, scale=True),
-    v2.Normalize(
-        mean=(0.4802, 0.4481, 0.3975),
-        std=(0.2764, 0.2689, 0.2816))
+    v2.Normalize(mean=(0.4802, 0.4481, 0.3975), std=(0.2764, 0.2689, 0.2816)),
 ]
 
 
-class CachedTinyImageNet(ImageFolder):
-
-    def __init__(self,
-                 root_dir: str,
-                 size: int,
-                 train: bool,
-                 transform: Optional[Callable],
-                 cache_size_limit_gb: int):
-        split = 'train' if train else 'test'
+class TinyImageNet(ImageFolder):
+    def __init__(
+        self,
+        root_dir: str,
+        train: bool,
+        transform: Optional[Callable],
+    ):
+        split = "train" if train else "test"
         tinyimagenet_root_dir = os.path.join(root_dir, split)
         super().__init__(tinyimagenet_root_dir, transform=transform)
-        num_items = super().__len__()
-        self.input_cache = SharedCache(
-            size_limit_gib=cache_size_limit_gb,
-            dataset_len=num_items,
-            data_dims=(3, size, size),
-            dtype=torch.float32)
 
     def __getitem__(self, idx: int) -> tuple[torch.Tensor, torch.Tensor]:
-        inputs = self.input_cache.get_slot(idx)
-        if inputs is None:
-            inputs, _ = super().__getitem__(idx)
-            self.input_cache.set_slot(idx, inputs)
+        inputs, _ = super().__getitem__(idx)
         labels = self.targets[idx]
         return (inputs, labels)
 
 
 @torch.no_grad
 def get_tiny_imagenet(
-        model_name: str,
-        data_root_dir: str,
-        cache_size_limit_gb: int) -> tuple[data.Dataset, data.Dataset]:
+    model_name: str, data_root_dir: str, cache_size_limit_gb: int
+) -> tuple[data.Dataset, data.Dataset]:
     model_name = model_name.lower()
-    tinyimagenet_root_dir = os.path.join(data_root_dir, 'tinyimagenet200')
-    if model_name == 'lenet':
+    tinyimagenet_root_dir = os.path.join(data_root_dir, "tinyimagenet200")
+    if model_name == "lenet":
         train_transforms = v2.Compose(_DEFAULT_TRAIN_TRANSFORMS)
         test_transforms = v2.Compose(_DEFAULT_TEST_TRANSFORMS)
-        train = CachedTinyImageNet(tinyimagenet_root_dir,
-                                   size=64,
-                                   train=True,
-                                   transform=train_transforms,
-                                   cache_size_limit_gb=cache_size_limit_gb)
-        test = CachedTinyImageNet(tinyimagenet_root_dir,
-                                  size=64,
-                                  train=False,
-                                  transform=test_transforms,
-                                  cache_size_limit_gb=cache_size_limit_gb)
+        train = TinyImageNet(
+            tinyimagenet_root_dir,
+            train=True,
+            transform=train_transforms,
+        )
+        test = TinyImageNet(
+            tinyimagenet_root_dir,
+            train=False,
+            transform=test_transforms,
+        )
         return (train, test)
-    elif model_name == 'alexnet':
+    elif model_name == "alexnet":
         train_transforms = v2.Compose(
-            [v2.Resize((224, 224))] + _DEFAULT_TRAIN_TRANSFORMS)
-        test_transforms = v2.Compose(
-            [v2.Resize((224, 224))] + _DEFAULT_TEST_TRANSFORMS)
-        train = CachedTinyImageNet(tinyimagenet_root_dir,
-                                   size=224,
-                                   train=True,
-                                   transform=train_transforms,
-                                   cache_size_limit_gb=cache_size_limit_gb)
-        test = CachedTinyImageNet(tinyimagenet_root_dir,
-                                  size=224,
-                                  train=False,
-                                  transform=test_transforms,
-                                  cache_size_limit_gb=cache_size_limit_gb)
+            [v2.Resize((224, 224))] + _DEFAULT_TRAIN_TRANSFORMS
+        )
+        test_transforms = v2.Compose([v2.Resize((224, 224))] + _DEFAULT_TEST_TRANSFORMS)
+        train = TinyImageNet(
+            tinyimagenet_root_dir,
+            train=True,
+            transform=train_transforms,
+        )
+        test = TinyImageNet(
+            tinyimagenet_root_dir,
+            train=False,
+            transform=test_transforms,
+        )
         return (train, test)
-    elif model_name == 'resnet18':
-        train_transforms = v2.Compose(
-            [v2.Resize((32, 32))] + _DEFAULT_TRAIN_TRANSFORMS)
-        test_transforms = v2.Compose(
-            [v2.Resize((32, 32))] + _DEFAULT_TEST_TRANSFORMS)
-        train = CachedTinyImageNet(tinyimagenet_root_dir,
-                                   size=32,
-                                   train=True,
-                                   transform=train_transforms,
-                                   cache_size_limit_gb=cache_size_limit_gb)
-        test = CachedTinyImageNet(tinyimagenet_root_dir,
-                                  size=32,
-                                  train=False,
-                                  transform=test_transforms,
-                                  cache_size_limit_gb=cache_size_limit_gb)
+    elif model_name == "resnet18":
+        train_transforms = v2.Compose([v2.Resize((32, 32))] + _DEFAULT_TRAIN_TRANSFORMS)
+        test_transforms = v2.Compose([v2.Resize((32, 32))] + _DEFAULT_TEST_TRANSFORMS)
+        train = TinyImageNet(
+            tinyimagenet_root_dir,
+            train=True,
+            transform=train_transforms,
+        )
+        test = TinyImageNet(
+            tinyimagenet_root_dir,
+            train=False,
+            transform=test_transforms,
+        )
         return (train, test)
-    raise NotImplementedError(f'TinyImageNet not available for {model_name}')
+    raise NotImplementedError(f"TinyImageNet not available for {model_name}")

tests/main.py
@@ -60,9 +60,7 @@ def get_train_optimizer(name: str, model: nn.Module, lr: float) -> optim.Optimiz
     elif name == "sgd":
         return optim.SGD(model.parameters(), lr=lr)
     elif name == "sgd_momentum":
-        return optim.SGD(
-            model.parameters(), lr=lr, momentum=0.9, nesterov=True, weight_decay=1e-4
-        )
+        return optim.SGD(model.parameters(), lr=lr, momentum=0.9, weight_decay=5e-4)
     raise NotImplementedError(f"{name} is not a supported post-prune Optimizier")
 
 
@@ -91,7 +89,7 @@ def main(args):
         alpha_id = f"{args.causal_pruner_l1_regularization_coeff}"
         prune_identifier += f"_{iteration_id}_{alpha_id}"
     elif prune_identifier == "magpruner":
-        prune_identifier += f"_{args.mag_pruner_amount}"
+        prune_identifier += f"_{args.total_prune_amount}"
     identifier = f"{model_name}_{dataset_name}_{prune_identifier}"
     suffix = args.suffix
     if suffix != "":
@@ -107,7 +105,6 @@ def main(args):
         dataset_name,
         model_name,
         args.dataset_root_dir,
-        cache_size_limit_gb=args.dataset_cache_size_limit_gb,
     )
     fabric = Fabric(devices=args.device_ids, accelerator="auto")
     fabric.launch()
@@ -212,7 +209,7 @@ def main(args):
                 start_clean=args.start_clean,
                 eval_after_epoch=args.eval_after_epoch,
                 reset_weights=args.reset_weights_after_pruning,
-                prune_amount=args.mag_pruner_amount,
+                prune_amount=prune_amount_per_iteration,
             )
         pruner = get_pruner(pruner_config)
     trainer = Trainer(trainer_config, pruner)
@@ -263,7 +260,7 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument(
         "--max_train_epochs",
         type=int,
-        default=200,
+        default=300,
         help="Maximum number of epochs for train the model",
     )
     parser.add_argument(
@@ -354,12 +351,6 @@ def parse_args() -> argparse.Namespace:
         help="Whether to shuffle the train and test datasets",
     )
     parser.add_argument("--batch_size", type=int, default=512, help="Batch size")
-    parser.add_argument(
-        "--dataset_cache_size_limit_gb",
-        type=int,
-        default=16,
-        help="Size limit for dataset stochastic cache",
-    )
     # Dirs
     parser.add_argument(
         "--checkpoint_dir",
@@ -454,7 +445,7 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument(
         "--causal_pruner_l1_regularization_coeff",
         type=float,
-        default=1e-3,
+        default=0,
         help="Causal Pruner L1 regularization coefficient",
     )
     parser.add_argument(
@@ -490,7 +481,7 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument(
         "--causal_pruner_pin_memory",
         action=argparse.BooleanOptionalAction,
-        default=True,
+        default=False,
         help="Controls if the causal pruner dataloader uses pinned memory.",
     )
     parser.add_argument(